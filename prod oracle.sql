9d43c7f708ed473f233d222ef10864fea1db12b3
Zaur Mamedrzaev <102297140+worrj03@users.noreply.github.com>
2024-01-24 10:22:02 +0300

    Update prod oracle.sql

--- prod oracle.sql
+++ prod oracle.sql
@@ -13,20 +13,20 @@ declare
     CURSOR cur1 IS
     select COLUMN_NAME
     from all_tab_columns
-    where owner = 'BUDM_VRB' AND table_name = 'VRB_DET_DEVICE'
+    where owner = 'schema' AND table_name = 'tbl'
     ORDER BY COLUMN_ID;
     
     CURSOR cur2 IS
     select column_name
     from all_tab_columns
-    where owner = 'BUDM_VRB' AND table_name = 'VRB_DET_DEVICE' AND DATA_TYPE = 'NUMBER' and column_name not like 'ID%'
+    where owner = 'schema' AND table_name = 'tbl' AND DATA_TYPE = 'NUMBER' and column_name not like 'ID%'
     ORDER BY COLUMN_ID;
     
 begin
     select table_name
     INTO l_tbl_name
     from all_tables
-    where owner = 'BUDM_VRB' AND table_name = 'VRB_DET_DEVICE';
+    where owner = 'schema' AND table_name = 'tbl';
     
     
     open cur1;
@@ -35,12 +35,12 @@ begin
         fetch cur1 into l_col_name_1;
         exit when cur1%notfound;
         
-        l_sql_1 := 'select count(' || l_col_name_1 || ') FROM BUDM_VRB.' || l_tbl_name;
-        l_sql_2 := 'select count(CASE WHEN ' || l_col_name_1 || ' IS NULL THEN 1 END) FROM BUDM_VRB.' || l_tbl_name;
+        l_sql_1 := 'select count(' || l_col_name_1 || ') FROM schema.' || l_tbl_name;
+        l_sql_2 := 'select count(CASE WHEN ' || l_col_name_1 || ' IS NULL THEN 1 END) FROM schema.' || l_tbl_name;
         
         execute immediate l_sql_1 into res_cnt;
         execute immediate l_sql_2 into res_null;
-        insert into CNT_ROWS_VRB  values (l_col_name_1, res_cnt, res_null);
+        insert into CNT_ROWS  values (l_col_name_1, res_cnt, res_null);
     end loop;
     close cur1;
     
@@ -52,24 +52,22 @@ begin
         fetch cur2 into l_col_name_2;
         exit when cur2%notfound;
         
-        l_sql_3 := 'select count( CASE when ' || l_col_name_2 || ' < 0 then 1 end) FROM BUDM_VRB.' || l_tbl_name;
+        l_sql_3 := 'select count( CASE when ' || l_col_name_2 || ' < 0 then 1 end) FROM schema.' || l_tbl_name;
         execute immediate l_sql_3 into res_neg;
-        insert into NEG_ROWS_VRB  values (l_col_name_2, res_neg);
+        insert into NEG_ROWS  values (l_col_name_2, res_neg);
     end loop;
     close cur2;
 end;
 
-drop table CNT_ROWS_VRB;
-drop table NEG_ROWS_VRB;
-create table INTECH.CNT_ROWS_VRB (name_table varchar2 (1000), cnt integer, cnt_null integer);
-create table INTECH.NEG_ROWS_VRB (name_table varchar2 (1000), neg integer);
+drop table CNT_ROWS;
+drop table NEG_ROWS;
+create table schema.CNT_ROWS (name_table varchar2 (1000), cnt integer, cnt_null integer);
+create table schema.NEG_ROWS (name_table varchar2 (1000), neg integer);
 on commit delete rows;
-TRUNCATE TABLE CNT_ROWS_VRB;
-TRUNCATE TABLE NEG_ROWS_VRB;
+TRUNCATE TABLE CNT_ROWS;
+TRUNCATE TABLE NEG_ROWS;
 
 SELECT CNT.NAME_TABLE, CNT.CNT, CNT.CNT_NULL, NEG.NEG
-FROM intech.CNT_ROWS_VRB cnt
-FULL JOIN intech.NEG_ROWS_VRB  neg on cnt.name_table = neg.name_table
-select *
-from NEG_ROWS_VRB
+FROM schema.CNT_ROWS cnt
+FULL JOIN schema.NEG_ROWS  neg on cnt.name_table = neg.name_table
 
